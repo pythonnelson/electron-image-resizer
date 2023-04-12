eef5315be4bbc09d8476a2eda8c87a43f2b493e9
Isaac NSB Kargbo <isaacnsbkargbo20@gmail.com>
2023-04-12 17:06:35 +0000

    Add index and about template pages

--- main.js
+++ main.js
@@ -1,21 +1,82 @@
 const path = require("path");
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Menu } = require("electron");
 
+const isDev = process.env.NODE_ENV !== "production";
 const isMac = process.platform === "darwin";
 
+// Creates the main window
 function createWindow() {
   const mainWindow = new BrowserWindow({
     title: "Electron Image Resizer",
-    width: 500,
+    width: isDev ? 1000 : 500,
     height: 600,
   });
+
+  //Open devtools if in development environment
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
 
   mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
 }
 
+// Open new window (About window)
+function createAboutWindow() {
+  const aboutWindow = new BrowserWindow({
+    title: "About Electron Image Resizer",
+    width: 300,
+    height: 300,
+  });
+
+  aboutWindow.loadFile(path.join(__dirname, "./renderer/about.html"));
+}
+
+// When app is ready
 app.whenReady().then(() => {
   createWindow();
+
+  // Implements menu
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
 });
+
+// Menu template
+const menu = [
+  ...(isMac
+    ? [
+        {
+          label: app.name,
+          submenu: [
+            {
+              label: "About",
+            },
+          ],
+        },
+      ]
+    : []),
+  {
+    role: "fileMenu",
+  },
+  ...(!isMac
+    ? [
+        {
+          label: "Help",
+          submenu: [
+            {
+              label: "About", // For windows and linux machine
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
+];
 
 app.on("window-all-closed", () => {
   if (!isMac) {
