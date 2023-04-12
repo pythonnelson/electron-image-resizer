610a9101bb176407c2984d4c3b5d14e6eb9b6ef3
Isaac NSB Kargbo <isaacnsbkargbo20@gmail.com>
2023-04-12 15:25:53 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const { app, BrowserWindow } = require("electron");
+
+const isMac = process.platform === "darwin";
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    title: "Electron Image Resizer",
+    width: 500,
+    height: 600,
+  });
+
+  mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+});
+
+app.on("window-all-closed", () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
